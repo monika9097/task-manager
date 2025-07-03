a5ae6896f8af918d4b5650d865681777c3e901bb
Kumari <monika.t.kumari@accenture.com>
2025-07-03 11:41:19 +0530

    Fix ObjectId serialization issue in Flask app

--- app/app.py
+++ app/app.py
@@ -1,24 +1,26 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from bson import ObjectId
 import os
 
 app = Flask(__name__)
 
-# MongoDB connection
 client = MongoClient(os.environ.get("MONGO_URI", "mongodb://localhost:27017/"))
 db = client["taskdb"]
 tasks_collection = db["tasks"]
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    tasks = list(tasks_collection.find({}, {"_id": 0}))
+    tasks = list(tasks_collection.find())
+    for task in tasks:
+        task['_id'] = str(task['_id'])
     return jsonify(tasks)
 
 @app.route('/tasks', methods=['POST'])
 def add_task():
-    data = request.get_json()
-    task = {"title": data["title"], "done": False}
-    tasks_collection.insert_one(task)
+    task = request.json
+    result = tasks_collection.insert_one(task)
+    task['_id'] = str(result.inserted_id)
     return jsonify(task), 201
 
 if __name__ == '__main__':

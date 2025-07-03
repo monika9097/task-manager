92b1dbc859c466440eef3983e9a03ef5d8f3f2b3
Kumari <monika.t.kumari@accenture.com>
2025-07-03 11:59:25 +0530

    Add simple UI and serve index.html

--- app/app.py
+++ app/app.py
@@ -1,26 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from pymongo import MongoClient
-from bson import ObjectId
 import os
 
 app = Flask(__name__)
 
+# MongoDB connection
 client = MongoClient(os.environ.get("MONGO_URI", "mongodb://localhost:27017/"))
 db = client["taskdb"]
 tasks_collection = db["tasks"]
 
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
+
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    tasks = list(tasks_collection.find())
-    for task in tasks:
-        task['_id'] = str(task['_id'])
+    tasks = list(tasks_collection.find({}, {"_id": 0}))
     return jsonify(tasks)
 
 @app.route('/tasks', methods=['POST'])
 def add_task():
     task = request.json
     result = tasks_collection.insert_one(task)
-    task['_id'] = str(result.inserted_id)
+    task['_id'] = str(result.inserted_id)  # Convert ObjectId to string
     return jsonify(task), 201
 
 if __name__ == '__main__':
